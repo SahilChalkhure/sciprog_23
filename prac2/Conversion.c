099f9d5b50656897feed74de2b307a884b228dfe
Sahil Chalkhure <sahilchalkhure26@gmail.com>
2023-11-26 15:22:53 +0000

    Practical2

---
+++ prac2/Conversion.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+/* Declare variables */
+   int i,inum,tmp,numdigits;
+   float fnum;
+   char binnum[60];
+
+/* Intialise 4-byte integer */
+   inum = 33554431;
+/* Convert to 4-byte float */
+   fnum = (float) inum;
+
+
+/* Convert to binary number (string)*/
+   i = 0; tmp = inum;
+   while (tmp > 0) {
+     sprintf(&binnum[i],"%1d",tmp%2);
+     tmp = tmp/2;
+     i++;
+   }
+
+/* Terminate the string */
+   binnum[i] = '\0';
+       
+
+/* fabs is used to calculate the log of the magnitude of the number */
+    numdigits = ceil( logf(fabs(inum)) + 1); // i used +1 for the sign bit
+    printf("The number of binary digits for %d is %d\n", inum, numdigits);
+
+
+
+   printf("inum=%d,  fnum=%f, inum in binary=%s\n",
+      inum,fnum,binnum);
+
+}
---
+++ prac2/prac2_Readme.txt
@@ -0,0 +1,28 @@
+Week 2 Practical
+
+
+For Q1. Conversion
+
+To Compile
+	 
+    gcc -o Conversion Conversion.c -lm -Wall
+
+To print
+
+    ./Conversion
+
+    (-lm is to map math library to the gcc and -Wall is to show all the warnings)
+
+
+For Q2. Sum of Series
+
+To Compile
+
+    gcc -o Sum Sum.c -Wall
+
+To print
+
+    ./Sum.c
+
+    
+
